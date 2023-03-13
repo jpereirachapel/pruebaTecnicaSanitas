3c5deb9787f75f5316fad4b791cfe02b1be0f338
Joaquin Pereira Chapel <streamtahlos@gmail.com>
2023-03-13 18:12:48 +0100

    Creacion app
    Creación de la aplicación en Spring

---
+++ sanitas/src/main/java/com/pruebatecnica/sanitas/sanitas/SanitasApplication.java
@@ -0,0 +1,13 @@
+package com.pruebatecnica.sanitas.sanitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanitasApplication.class, args);
+	}
+
+}
